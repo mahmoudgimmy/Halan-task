19d1fe5a052da46f590486aba7e88bf450334c96
Mahmoud Gamal <mahmoud.gamal2791996@gmail.com>
2021-11-23 01:12:20 +0200

    - add sharedPrefModule to koin modules in MainApplication

--- app/src/main/java/com/example/halanchallenge/MainApplication.kt
+++ app/src/main/java/com/example/halanchallenge/MainApplication.kt
@@ -3,20 +3,26 @@ package com.example.halanchallenge
 import android.app.Application
 import com.example.halanchallenge.di.networkModule
 import com.example.halanchallenge.di.repositoriesModule
+import com.example.halanchallenge.di.sharedPrefModule
 import com.example.halanchallenge.di.viewModelsModule
+import okhttp3.internal.immutableListOf
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class MainApplication: Application() {
+class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidLogger()
             androidContext(this@MainApplication)
             modules(
-                listOf(networkModule, viewModelsModule, repositoriesModule)
-
+                listOf(
+                    networkModule,
+                    viewModelsModule,
+                    repositoriesModule,
+                    sharedPrefModule
+                )
             )
         }
     }
