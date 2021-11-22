699c8b41874d54151810d790cc70c9048f0e9617
Mahmoud Gamal <mahmoud.gamal2791996@gmail.com>
2021-11-23 01:19:13 +0200

    - move ProductListActivity.kt - save user token in LoginActivity using sharedPreferences

--- app/src/main/java/com/example/halanchallenge/ui/login/activities/LoginActivity.kt
+++ app/src/main/java/com/example/halanchallenge/ui/login/activities/LoginActivity.kt
@@ -1,20 +1,25 @@
 package com.example.halanchallenge.ui.login.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
+import com.example.halanchallenge.data.local.SharedPreferencesManager
 import com.example.halanchallenge.databinding.ActivityLoginBinding
 import com.example.halanchallenge.model.LoginCredentials
 import com.example.halanchallenge.ui.login.viewmodels.LoginViewModel
 import com.example.halanchallenge.ui.login.viewmodels.LoginViewState
+import com.example.halanchallenge.ui.productslist.activities.ProductListActivity
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val viewModel: LoginViewModel by viewModel()
+    private val sharedPref: SharedPreferencesManager by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +31,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initializeObservers() {
-        viewModel.userData.observe(this, Observer {
+        viewModel.userData.observe(this, {
             renderUi(it)
         })
     }
@@ -37,7 +42,16 @@ class LoginActivity : AppCompatActivity() {
                 is LoginViewState.LOADING -> pbLoading.isVisible = true
                 is LoginViewState.SUCCESS -> {
                     pbLoading.isVisible = false
-                    viewState.payload
+                    sharedPref.saveToken(viewState.payload.token)
+
+                    startActivity(
+                        Intent(
+                            this@LoginActivity,
+                            ProductListActivity::class.java
+                        ).apply {
+                            putExtra("Profile", viewState.payload.profile)
+                        })
+                    finish()
                 }
 
                 is LoginViewState.FAILURE -> {
--- app/src/main/java/com/example/halanchallenge/ui/login/repositories/LoginRepository.kt
+++ app/src/main/java/com/example/halanchallenge/ui/login/repositories/LoginRepository.kt
@@ -2,7 +2,7 @@ package com.example.halanchallenge.ui.login.repositories
 
 import com.example.halanchallenge.model.LoginCredentials
 import com.example.halanchallenge.model.UserData
-import com.example.halanchallenge.remote.HalanRemote
+import com.example.halanchallenge.data.remote.HalanRemote
 
 class LoginRepository(private val halanRemote: HalanRemote):ILoginRepository {
     override suspend fun login(loginCredentials: LoginCredentials): UserData = halanRemote.login(loginCredentials)
