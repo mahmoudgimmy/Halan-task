17c12cfd54c616198621a940503c143592eb27cf
Mahmoud Gamal <mahmoud.gamal2791996@gmail.com>
2021-11-23 01:13:32 +0200

    - add BindingAdapters with load image method to use dataBinding to load it

---
+++ app/src/main/java/com/example/halanchallenge/utilities/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.example.halanchallenge.utilities
+
+import android.widget.ImageView
+import android.widget.ListAdapter
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+// xml attribute to load images using dataBinding (in xml itself)
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(view.context).load(url).into(view)
+    }
+}
